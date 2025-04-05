17de7ed2227539988ee86da89aba9c8b713ee184
texascake <tiktodz@soulvibe-team.cloud>
2025-04-05 03:35:45 +0000

    Revert "fs: drop get_ksu_state definition"
    This reverts commit c0d278fca4e016ecc22125197af28cf98bcee68c.
    
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- fs/stat.c
+++ fs/stat.c
@@ -102,7 +102,8 @@ int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 	int error = -EINVAL;
 	unsigned int lookup_flags = 0;
 
-#ifdef CONFIG_KSU 
+#ifdef CONFIG_KSU
+	if (get_ksu_state() > 0)
 	ksu_handle_stat(&dfd, &filename, &flag);
 #endif
 
